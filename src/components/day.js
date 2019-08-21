6605c6042ca9e3e7d43c51848b4fe5002257ec15
iu-shvetsova <iu-shvetsova69@yandex.ru>
2019-08-21 13:36:22 +0300

    Доработан модуль

--- src/components/day.js
+++ src/components/day.js
@@ -1,8 +1,8 @@
 export const getDayComponent = (number, date) => `
-  <li class="trip-days__item  day">
+  <li class="trip-days__item day">
     <div class="day__info">
       <span class="day__counter">${number}</span>
-      <time class="day__date" datetime="2019-03-18">${new Date(date).toDateString().slice(-11, -4)}</time>
+      <time class="day__date" datetime="${new Date(date).toISOString().slice(0, 16)}">${new Date(date).toDateString().slice(-11, -4)}</time>
     </div>
   </li>
 `;
