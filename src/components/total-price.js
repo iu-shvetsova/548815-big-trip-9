8030afa360e07f8417e75f5970410d30b869805a
iu-shvetsova <iu-shvetsova69@yandex.ru>
2019-08-22 23:48:23 +0300

    Исправлена функция расчёта итоговой цены

--- src/components/total-price.js
+++ src/components/total-price.js
@@ -2,7 +2,9 @@ export const getTotalPriceComponent = (events) => `
   <p class="trip-info__cost">
     Total: &euro;&nbsp;<span class="trip-info__cost-value">${events.reduce((acc, event) => {
     acc += event.price;
-    event.options.filter((item) => item[`isChecked`]).forEach((item) => acc += item[`price`]);
+    event.options.filter((item) => item[`isChecked`]).forEach((item) => {
+      acc += item[`price`];
+    });
     return acc;
   }, 0)}
     </span>
